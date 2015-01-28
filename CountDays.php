76fad4eb6a7b23428e6353fe7107ac48ec25427d
Anthony Rodriguez <anthonyrodriguez.itt@gmail.com>
2015-01-28 18:46:04 -0500

    CountDays algorithm

---
+++ CountDays.php
@@ -0,0 +1,24 @@
+<?php 
+function countDays($dateInString){
+
+	// if the format is valid
+	if(preg_match("/^\d{2}\.\d{2}\.\d{4}$/",$dateInString)){
+		$dateArr = explode('.',$dateInString);
+
+		$validDate = strtotime($dateArr[2].$dateArr[0].$dateArr[1]);
+		$firstDay =  strtotime($dateArr[2]."01"."01");
+		$secs =$validDate-$firstDay;
+
+		$days = $secs/(60*60*24);
+
+		print $days;
+		
+	}
+
+
+
+	
+
+}
+
+countDays("02.01.2015");
